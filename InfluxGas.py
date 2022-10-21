f00600d691d89c5859ce29d804040faf2cdc9fc4
mjtkeijsers <mjt.keijsers@gmail.com>
2022-10-21 15:22:58 +0200

    minor

--- InfluxGas.py
+++ InfluxGas.py
@@ -55,7 +55,7 @@ print(gas_n1_str)
 gas_n0_f = extract_value("gas",gas_n0_str)
 gas_n1_f = extract_value("gas",gas_n1_str)
         
-gas_last_minute = gas_n0_f - gas_n1_f
+gas_m3_hr = (gas_n0_f - gas_n1_f) * 6.0 #To convert a 10 min sample to m3/h
 
 # take a timestamp for this measurement
 time = datetime.datetime.utcnow()
@@ -66,7 +66,7 @@ body = [
         "measurement": measurement_name,
         "time": time,
         "fields": {
-            "gas_last_min": gas_last_minute
+            "gas_m3_hr": gas_m3_hr
         }
     }
 ]
